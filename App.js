94a74f30766147176ea35641cb06a4bac8e96f85
EricHsia <hsiayinghao1818@gmail.com>
2023-02-11 19:20:57 -0800

    heyhey 123

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 // Test from Eric
 // my local change
+// heyhey 123
 export default function App() {
   return (
     <View style={styles.container}>
