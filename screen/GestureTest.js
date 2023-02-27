186626492ae50f4319860527cc5f8367fe94b922
林政寬 LIN ZHENG KUAN <jimmy8604168@gmail.com>
2023-02-27 13:30:45 +0800

    Merge branch 'main' of https://github.com/JENNSHIUAN/MRT-for-Foodies
    replace Eric code??

--- screen/GestureTest.js
+++ screen/GestureTest.js
@@ -1,7 +1,10 @@
-import { Dimensions, StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet, View, Animated } from 'react-native';
 import {
-  GestureHandlerRootView,
-} from 'react-native-gesture-handler';
+    Gesture,
+    GestureDetector,
+    GestureHandlerRootView,
+  } from 'react-native-gesture-handler';
+//import { useSharedValue } from 'react-native-reanimated';
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
@@ -9,10 +12,27 @@ const SIZE = 80;
 
 export default function App() {
 
+  const translateX = useSharedValue(0)
+  const translateY = useSharedValue(0)
+  
+  const gesture = Gesture.Pan().onUpdate((event) => {
+    translateX.value = event.translationX
+    translateY.value = event.translationY
+  })
+  
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: translateX.value }, { translateY: translateY.value }],
+    }
+  })
+
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <View style={styles.circle} />
+        <GestureDetector gesture={gesture}>
+          <Animated.View style={[styles.circle, rStyle]} />
+        </GestureDetector>
       </View>
     </GestureHandlerRootView>
   );
